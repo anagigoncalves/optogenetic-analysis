9a01167604defa20820562013c6c19ea5c9d890c
“AliceGem“ <alice.geminiani@polimi.it>
2026-06-04 12:27:48 +0100

    Added pixel_to_mm accounting for different setups in gait param comput

--- tied_behavior_analysis_single_animal.py
+++ tied_behavior_analysis_single_animal.py
@@ -27,15 +27,17 @@ frames_dFF = 0 #black frames removed before ROI segmentation
 start_split = 9
 
 
-
-#TODO: add pixel to mm for the two setups!!!!!!!!!!!!!!!!!!!!!!!!
-
-
+pixel_to_mm = 1/3.3            # real-time setup
+floor_factor = 268
+if 'WT' in path_loco:
+    #pixel_to_mm = 1/1.955           # Dana
+    pixel_to_mm = 1/1.98           # Jovin
+    floor_factor = 152  
 
 #import classes
 #os.chdir('C:\\Users\\Ana\\Documents\\PhD\\Dev\\optogenetic-analysis\\')
 import locomotion_class
-loco = locomotion_class.loco_class(path_loco)
+loco = locomotion_class.loco_class(path_loco, pixel_to_mm=pixel_to_mm, floor_factor=floor_factor)
 if not os.path.exists(path_loco+'gait parameters'):
     os.mkdir(path_loco+'gait parameters')
 
